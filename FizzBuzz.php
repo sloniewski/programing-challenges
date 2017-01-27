7117df525e6bdd5dc6de63f1f412094adb84271a
sloniewski <sloniewski.maciej@gmail.com>
2017-01-28 00:01:47 +0100

    .

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -2,10 +2,7 @@
 for($i=1;$i<100;$i++){
   if (($i%3)!=0 && ($i%5)!=0) echo $i;
   if (($i%3)==0) echo "Fizz";
-  if (($i%5)==0)echo "BUzz";
+  if (($i%5)==0) echo "Buzz";
   echo "<br>";
 }
-
-
-
- ?>
+?>
