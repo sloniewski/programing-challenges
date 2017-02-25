f9caa3f74ecf7a4a0eda5a61bb322ce57b31f150
Maciej <sloniewski.maciej@gmail.com>
2017-02-25 14:13:54 +0100

    fixed loop

--- Programing_challenges.php
+++ Programing_challenges.php
@@ -10,12 +10,11 @@ for($i=1;$i<15;$i++){
 //16 Reverse a string
 $string = "abcdefghij";
 function reverse($string){
-for ( $i=0 ; $i< (strlen($string)) ; $i++){
   $str_reversed = "";
-  $letter = substr($string,(strlen($string)-$i),1);
-  $str_reversed .= $letter;
-  return $str_reversed;
-}}
+  for ( $i=0 ; $i< (strlen($string)) ; $i++){
+    $letter = substr($string,(strlen($string)-$i),1);
+    $str_reversed .= $letter;}
+  return $str_reversed;}
 $reversed = reverse($string);
 echo $reversed;
 $a = substr($string, (strlen($string)-4),1);
