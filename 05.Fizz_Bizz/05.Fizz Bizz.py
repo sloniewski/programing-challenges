267680623ded989a983d1b62df941b85cf1f4549
Maciej <sloniewski.maciej@gmail.com>
2017-04-05 23:22:04 +0200

    .

---
+++ 05.Fizz_Bizz/05.Fizz Bizz.py
@@ -0,0 +1,10 @@
+def FizzBizz(n):
+    x = 0
+    while x < n:
+        x = x + 1
+        if x % 3 != 0 and x % 5 != 0:
+            print x
+        if x % 3 == 0:
+            print 'Fizz'
+        if x % 5 == 0:
+            print 'Bizz'
