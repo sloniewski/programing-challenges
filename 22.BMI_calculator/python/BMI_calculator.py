f8ddfbb3a316ab0c00219051012eb1839431d0c9
sloniewski <sloniewski.maciej@gmail.com>
2017-04-08 02:19:49 +0200

    .

--- 22.BMI_calculator/python/BMI_calculator.py
+++ 22.BMI_calculator/python/BMI_calculator.py
@@ -1,3 +1,12 @@
-print ('Please eneter your')
-weight = input('weight in kilograms ')
-height = input('height in centimiters ')
+import re
+print ('Please eneter your:')
+weight = input('weight in kilograms: ')
+height = input('height in centimiters: ')
+match = re.match('[a-zA-Z]*$',weight)
+if match:
+    print ('Numbers only allowed')
+    weight = input('weight in kilograms: ')
+weight = int(weight)
+height = int(height)
+BMI = weight / ((height/100)**2)
+print (BMI)
