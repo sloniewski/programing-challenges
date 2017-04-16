a1168f9597c5ed5b677b77af89e005133e80f2c4
Maciek Słoniewski <sloniewski.maciej@gmail.com>
2017-04-16 22:55:59 +0200

    a commit with command line

--- 57.Sorting_algorythms/PHP/Insert_sort.php
+++ 57.Sorting_algorythms/PHP/Insert_sort.php
@@ -2,9 +2,9 @@
 //Functions separated to be reused
 function minimum($array) //get minimum value of array
 {
-    $output=$array[0]; //define first element of array as output
+    $output = $array[0]; //define first element of array as output
     foreach ($array as $input) {
-        if ($output>$input) $output = $input;	//compare each element of array outpuy variable, if smaller than swith with output var
+        if ($output > $input) $output = $input;	//compare each element of array outpuy variable, if smaller than swith with output var
     }
     return $output;
 }
@@ -26,14 +26,14 @@ function removeFromArray($array,$number) //this function removes a single given 
 function insertSort($array)
 {
     $array_input = $array;
-    if (count($array_input)>1) {
+    if (count($array_input) > 1) {
         foreach($array_input as $element){
             $minimum = minimum($array_input); //find minimum of input array
-            $array_output[]=$minimum; //add minimum to new output array
-            $array_input=removeFromArray($array_input,$minimum); //remove minimum from input array, create new input array
+            $array_output[] = $minimum; //add minimum to new output array
+            $array_input = removeFromArray($array_input,$minimum); //remove minimum from input array, create new input array
         }
     } else {
-        $array_output[]=$array_input[0];
+        $array_output[] = $array_input[0];
     }
     return $array_output;
 }
