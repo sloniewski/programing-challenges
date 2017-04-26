5433565496ecd0b6e3b729d340bc45f9035596c9
sloniewski <sloniewski.maciej@gmail.com>
2017-04-26 21:04:16 +0200

    super factorial

--- 29.Factorial/factorial.php
+++ 29.Factorial/factorial.php
@@ -6,4 +6,28 @@ function factorial($number)
     }
     return ($number * factorial($number-1));
 }
+
+
+function superFactorial($number)
+{
+    $array_temp = [];
+    while ($number > 0) {
+        $array_temp[] = $number;
+        --$number;
+    }
+
+    array_walk(
+        $array_temp,
+        function(&$x) {$x = factorial($x);}
+    );
+
+    $result = 1;
+
+    foreach ($array_temp as $element){
+        $result = $result * $element;
+    }
+
+    return ($result);
+}
+
 ?>
